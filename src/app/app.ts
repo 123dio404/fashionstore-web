8019091b6604e065e28b4b3871748527669f2edb
Axelmultidades <aaxel532@gmail.com>
2026-09-05 02:06:48 -0400

    interfas de prueba

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [RouterLink]
 })
 export class App {
 }
